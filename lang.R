1e4ffc38bcd687c6ae039ef5fc8ece09c930871c
liulang666666 <lang.js.liu@gmail.com>
2020-02-22 15:04:02 -0500

    plot

--- lang.R
+++ lang.R
@@ -1,9 +1,30 @@
 library(readxl)
 library(caret)
 library(dplyr)
+library(ggplot2)
+
 df <- read_xlsx("regen20-dataset.xlsx")
 df %>% head()
 colnames(df)
+
+#check if there is any missing data
+df %>% is.na() %>%
+  sum()
+
+#check the distribution of age
+summary(as.factor(df$age))
+df$age <- as.factor(df$age)
+
+#check the number of patients
+summary(as.factor(df$target))
+df$target <- as.factor(df$target)
+
+
+#look at how cardiovascular disease is spread among people's age
+g <- ggplot(df, aes(age, fill = target))
+g + geom_bar(position = 'dodge') +
+  labs(title = 'age vs target') +
+  scale_fill_manual(labels = c('healthy','disease'), values = c('deepskyblue1', 'orangered2'))
 
 df$target <- as.factor(df$target)
 intrain <- createDataPartition(y = df$target, p= 0.7, list = FALSE)
