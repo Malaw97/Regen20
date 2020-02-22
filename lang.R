b6872ce1b87c10efbca0918dcd73bc4e20363a72
liulang666666 <lang.js.liu@gmail.com>
2020-02-22 16:19:41 -0500

    svm Naive Bayes decision tree random forest

--- lang.R
+++ lang.R
@@ -26,6 +26,8 @@ g + geom_bar(position = 'dodge') +
   labs(title = 'age vs target') +
   scale_fill_manual(labels = c('healthy','disease'), values = c('deepskyblue1', 'orangered2'))
 
+
+#svm
 df$target <- as.factor(df$target)
 intrain <- createDataPartition(y = df$target, p= 0.7, list = FALSE)
 training <- df[intrain,]
@@ -66,3 +68,42 @@ test_pred_grid <- predict(svm_Linear_Grid, newdata = testing)
 test_pred_grid
 
 confusionMatrix(table(test_pred_grid, testing$target))
+
+
+#naive Bayesian
+library(e1071)
+#dont factorize age in this model
+Naive_Bayes_Model = naiveBayes(target ~.,data = training)
+Naive_Bayes_Model
+NB_Predictions = predict(Naive_Bayes_Model, testing)
+confusionMatrix(table(NB_Predictions,testing$target))
+
+
+#decision tree
+library(rpart.plot)
+df %>% head()
+#do we need to normalize continuous data and as.factor() categorical data?
+trctrl <- trainControl(method = 'repeatedcv', number = 10, repeats = 3)
+set.seed(3333)
+dtree_fit <- train(target ~.,data = training, method = 'rpart',
+                   parms = list(split = 'information'),
+                   trControl = trctrl,
+                   tuneLength = 10)
+dtree_fit
+prp(dtree_fit$finalModel, box.palette = 'Reds', tweak = 1.2)
+
+test_pred_dt <- predict(dtree_fit, newdata= testing)
+confusionMatrix(test_pred_dt,testing$target)
+
+
+
+#random forest
+library(randomForest)
+set.seed(100)
+rf <- randomForest(target ~., data = training, importance = TRUE, ntree = 500, mtry = 6)
+rf
+predTrain <- predict(rf, training, type = 'class')
+table(predTrain,training$target)
+predtest <- predict(rf,testing, type = 'class')
+table(predtest, testing$target)
+confusionMatrix(table(predtest, testing$target))
